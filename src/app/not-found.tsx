3e3a8cd82e637ed51a526c6bfc548381fe4c05ef
Suphachock Kamlek <suphachock.dev@gmail.com>
2024-12-15 13:06:06 +0700

    feat: add page handle 404 page not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,44 @@
+"use client";
+import SnowFallBackground from "@/components/snowFallBackground";
+import Link from "next/link";
+import { motion } from "motion/react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+	return (
+		<div className="w-full min-h-screen h-full flex flex-col items-center justify-center bg-gradient-to-b from-blue-800 to-black px-6">
+			<SnowFallBackground />
+			<div className="absolute flex flex-col items-center justify-center space-y-4">
+				<motion.div
+					className=""
+					initial={{ y: 0 }} // Initial position
+					animate={{
+						y: [0, -20, 0], // Move up and down
+					}}
+					transition={{
+						duration: 3, // Total animation time
+						repeat: Infinity, // Loop animation forever
+						ease: "easeInOut", // Smooth movement
+					}}
+				>
+					<Image
+						src={"/image/christmas-mascot.png"}
+						alt="Christmas mascot"
+						width={200}
+						height={200}
+					/>
+				</motion.div>
+				<h3 className="text-white text-4xl font-bold">404</h3>
+				<h3 className="text-white text-lg font-bold">
+					ไม่พบหน้าที่ต้องการเข้าใช้งาน 😿
+				</h3>
+				<Link href={"/home"}>
+					<Button className="bg-white text-black hover:bg-white/80">
+						กลับสู่หน้าหลัก
+					</Button>
+				</Link>
+			</div>
+		</div>
+	);
+}
